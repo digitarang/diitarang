ca1e8f5e50f29861a927c882e1cfbcded58123ac
DigiTarang <76580553+digitarang@users.noreply.github.com>
2021-06-06 03:45:04 +0530

    Update digitarang.js

--- scripts/digitarang.js
+++ scripts/digitarang.js
@@ -1,1 +1,16 @@
 
+var hasUpdates = false;
+[].forEach.call(document.querySelectorAll("pre code"), function(el) {
+  hasUpdates = true;
+  el.classList.add("prettyprint");
+});
+
+if (hasUpdates) {
+  var link = document.createElement('link');
+  link.type = 'text/css'
+  link.rel = 'stylesheet'
+  link.href = '//cdn.jsdelivr.net/gh/google/code-prettify@master/loader/prettify.css'
+
+  document.head.appendChild(link)
+  prettyPrint();
+}
